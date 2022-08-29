6759d8cc2d0b4c1463997848306961fca7e28e84
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-29 08:34:22 +0200

    chore: refactor test code

--- lib/src/test/java/travel_price_calculator/TravelPriceCalculatorWithMocksTest.java
+++ lib/src/test/java/travel_price_calculator/TravelPriceCalculatorWithMocksTest.java
@@ -3,6 +3,8 @@ package travel_price_calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.mockito.Mockito.*;
 
 public class TravelPriceCalculatorWithMocksTest {
@@ -17,8 +19,10 @@ public class TravelPriceCalculatorWithMocksTest {
     @Test
     void verify_interaction_between_travelTimeCalculatorMock_and_travelRateRepositoryMock_with_getTravelTime_and_getTravelRate() {
         String travelId = "1456";
-        travelPriceCalculator.getPrice(travelId);
-        verify(travelTimeCalculatorMock).getTravelTime(travelId);
+        int travelTime = 900;
+        HashMap<String, Boolean> discount = chooseDiscount(false, false, false);
+        travelPriceCalculator.getPrice(travelId, travelTime, discount);
+        verify(travelTimeCalculatorMock).getTravelTime(travelId,travelTime);
         verify(travelRateRepositoryMock).getTravelRate(travelId);
 
     }
@@ -26,21 +30,34 @@ public class TravelPriceCalculatorWithMocksTest {
     @Test
     void verify_interaction_between_travelDiscountRepositoryMock_with_getTravelDiscount() {
         String travelId = "1456";
-        travelPriceCalculator.getPrice(travelId);
-        verify(travelTimeCalculatorMock).getTravelTime(travelId);
+        int travelTime = 900;
+        boolean discountInTravelTime = true;
+        boolean discountInTravelRate = false;
+        HashMap<String, Boolean> discount = chooseDiscount(true, false, false);
+        travelPriceCalculator.getPrice(travelId, travelTime, discount);
+        verify(travelTimeCalculatorMock).getTravelTime(travelId, travelTime);
         verify(travelRateRepositoryMock).getTravelRate(travelId);
-        verify(travelDiscountRepositoryMock).getTravelDiscount(travelId);
+        verify(travelDiscountRepositoryMock).getTravelTimeDiscount(travelId);
     }
 
     @Test
     void calculate_travel_rate_with_travelTimeCalculatorMock_and_travelRateRepositoryMock() {
         String travelId = "1398";
-        travelPriceCalculator.getPrice(travelId);
-        when(travelTimeCalculatorMock.getTravelTime(travelId)).thenReturn(7);
+        int travelTime = 900;
+        HashMap<String, Boolean> discount = chooseDiscount(false, false, false);
+        travelPriceCalculator.getPrice(travelId, travelTime, discount);
+        when(travelTimeCalculatorMock.getTravelTime(travelId, travelTime)).thenReturn(7);
         when(travelRateRepositoryMock.getTravelRate(travelId)).thenReturn(1.3);
-        Double actualResult = travelTimeCalculatorMock.getTravelTime(travelId) * travelRateRepositoryMock.getTravelRate(travelId);
+        Double actualResult = travelTimeCalculatorMock.getTravelTime(travelId, travelTime) * travelRateRepositoryMock.getTravelRate(travelId);
         Double expectedResult = 9.1;
         Assertions.assertThat(actualResult).isEqualTo(expectedResult);
 
+    }
+    private HashMap<String, Boolean> chooseDiscount(Boolean travelTimeDiscount, Boolean travelRateDiscount, Boolean ticket) {
+        HashMap<String, Boolean> discounts = new HashMap<>();
+        discounts.put("travel time discount", travelTimeDiscount);
+        discounts.put("travel rate discount", travelRateDiscount);
+        discounts.put("ticket", ticket);
+        return discounts;
     }
 }
